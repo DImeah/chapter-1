d5d5363d46e78df7bbfee0c892ff5fad77530f8a
Imeah Dornubari <imeahdornu@gmail.com>
2026-05-23 17:23:43 +0100

    Used cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\Users\user\Documents\Learning Projects\Rust\chapter 1\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+C:\Users\user\Documents\Learning Projects\Rust\chapter 1\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\user\Documents\Learning\ Projects\Rust\chapter\ 1\hello_cargo\target\debug\hello_cargo.exe: C:\Users\user\Documents\Learning\ Projects\Rust\chapter\ 1\hello_cargo\src\main.rs
